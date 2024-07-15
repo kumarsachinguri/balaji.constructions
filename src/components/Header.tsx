17c7b8699e31ad281927e9b39e227381b3368e35
kumarsachinguri <kumarsachinguri@gmail.com>
2024-07-15 12:59:31 +0530

    Add tailwind css NavBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,96 @@
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 const Header = () => {
-  const links: { label: string; route: string }[] = [
-    { label: "Home", route: "/home" },
-    { label: "Portfolio", route: "/portfolio" },
-    { label: "Contact", route: "/contact" },
+  const navigation = [
+    { name: "Home", href: "/home", current: true },
+    { name: "Portfolio", href: "/portfolio", current: false },
+    { name: "Contact", href: "/contact", current: false },
   ];
-  const listItems = links.map((link) => (
-    <li key={link.label}>
-      <a>{link.label}</a>
-    </li>
-  ));
+
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   return (
     <header className='bg-amber-50'>
-      <div className='flex items-center gap-2 p-4 mx-auto maxwidth'>
-        <img src='images/logo-bc.png' alt='Logo' className='w-16' />
-        <ul className='list-none flex items-center gap-2 text-slate-900 ms-auto'>
-          {listItems}
-        </ul>
-      </div>
+      <Disclosure as='nav' className='bg-amber-50'>
+        <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+          <div className='relative flex h-16 items-center justify-between'>
+            <div className='absolute inset-y-0 right-0 flex items-center sm:hidden'>
+              {/* Mobile menu button*/}
+              <DisclosureButton className='group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-amber-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                <span className='absolute -inset-0.5' />
+                <span className='sr-only'>Open main menu</span>
+                <Bars3Icon
+                  aria-hidden='true'
+                  className='block h-6 w-6 group-data-[open]:hidden'
+                />
+                <XMarkIcon
+                  aria-hidden='true'
+                  className='hidden h-6 w-6 group-data-[open]:block'
+                />
+              </DisclosureButton>
+            </div>
+            <div className='flex items-center justify-between w-full'>
+              <div className='flex flex-shrink-0 items-center'>
+                <img src='images/logo-bc.png' alt='Logo' className='w-10' />
+                <p className='flex flex-col'>
+                  <strong className='block font-black leading-4 text-red-600'>
+                    Bala ji
+                  </strong>
+                  <strong className='block font-black leading-4 text-green-500'>
+                    Constructions
+                  </strong>
+                </p>
+              </div>
+              <div className='hidden sm:ml-6 sm:block ms-auto'>
+                <div className='flex space-x-4'>
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      aria-current={item.current ? "page" : undefined}
+                      className={classNames(
+                        item.current
+                          ? "text-amber-400"
+                          : "text-slate-400 hover:text-amber-300",
+                        "rounded-md px-3 py-2 text-sm font-bold"
+                      )}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <DisclosurePanel className='sm:hidden'>
+          <div className='space-y-1 px-2 pb-3 pt-2'>
+            {navigation.map((item) => (
+              <DisclosureButton
+                key={item.name}
+                as='a'
+                href={item.href}
+                aria-current={item.current ? "page" : undefined}
+                className={classNames(
+                  item.current
+                    ? "text-amber-400"
+                    : "text-gray-300 hover:text-amber-300",
+                  "block rounded-md px-3 py-2 text-base font-medium"
+                )}
+              >
+                {item.name}
+              </DisclosureButton>
+            ))}
+          </div>
+        </DisclosurePanel>
+      </Disclosure>
     </header>
   );
 };
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,7 +2,7 @@ import Icon from "../core/Icon";
 
 const TopBar = () => {
   return (
-    <div className='w-full flex items-center justify-center gap-4 bg-amber-400 text-sm p-2'>
+    <div className='w-full flex items-center flex-wrap justify-center gap-4 bg-amber-400 text-sm p-2'>
       <a href='tel:+91 83075 29392' className='flex items-center gap-1'>
         <Icon name={"smartphone"} size={1}></Icon>
         83075-29392
