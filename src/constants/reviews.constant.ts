f2727262bfe6c7c33961eb49ab73079e006bde3c
kumarsachinguri <kumarsachinguri@gmail.com>
2024-07-16 11:31:25 +0530

    Remove extra images and fix deploy path

--- src/constants/reviews.constant.ts
+++ src/constants/reviews.constant.ts
@@ -2,13 +2,13 @@ import { Review } from "../model/Review";
 
 export const REVIEW_LIST: Review[] = [
   {
-    fullName: "DEVENDER SINGH",
+    fullName: "Sachin Kumar",
     position: "Project Manager",
     review:
       "A myriad of products and design options to choose from, we were delighted to have our project done by Balaji. We recommend them for their professional competence in executing projects, end-to-end",
   },
   {
-    fullName: "SONU YADAV",
+    fullName: "Subash Verma",
     position: "Construct Chief",
     review:
       "Thank you Balaji Constructions for giving us quality roofing through sandwich panels for our schools. The insulation offered by the panels particularly that of temperature control has been of immense benefit to our students and staff alike, especially at our location with semi-arid desert-like weather conditions.",
