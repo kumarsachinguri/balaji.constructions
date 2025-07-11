e77483023b174d61b283339f7dbd8e8a1fbe775a
kumarsachinguri <kumarsachinguri@gmail.com>
2025-07-11 13:29:08 +0530

    name correction

--- src/constants/reviews.constant.ts
+++ src/constants/reviews.constant.ts
@@ -8,7 +8,7 @@ export const REVIEW_LIST: Review[] = [
       "A myriad of products and design options to choose from, we were delighted to have our project done by Balaji. We recommend them for their professional competence in executing projects, end-to-end",
   },
   {
-    fullName: "Subash Verma",
+    fullName: "Subhash Verma",
     position: "Construct Chief",
     review:
       "Thank you Balaji Constructions for giving us quality roofing through sandwich panels for our schools. The insulation offered by the panels particularly that of temperature control has been of immense benefit to our students and staff alike, especially at our location with semi-arid desert-like weather conditions.",
